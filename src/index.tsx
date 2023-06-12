92b011f2e75d449ca6bf055a7fa6d2d6753ee01d
guimendel <guimendeln@gmail.com>
2023-06-12 17:46:59 -0300

    🔨Scaffold Dynamic login

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { DynamicContextProvider } from '@dynamic-labs/sdk-react'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
-    <App />
+    <DynamicContextProvider
+      settings={{ environmentId: 'b8318da7-07b5-47f9-9b40-ed48bc59d430' }}
+    >
+      <App />
+    </DynamicContextProvider>
   </React.StrictMode>
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
