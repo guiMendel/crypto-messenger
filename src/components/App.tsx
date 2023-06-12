5c31596285ca524031fd113c2bf60704ce2f66a4
guimendel <guimendeln@gmail.com>
2023-06-12 19:51:35 -0300

    🔨Integrate XMTP and Dynamic

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,22 @@
-import { DynamicWidget } from '@dynamic-labs/sdk-react';
+import { DynamicWidget } from '@dynamic-labs/sdk-react'
+import { useMessenger } from '../modules/useMessenger'
+import { useEffect } from 'react'
 
 function App() {
+  const messenger = useMessenger()
+
+  useEffect(() => {
+    messenger?.conversations
+      .list()
+      .then((conversations) => console.log('Conversations:', conversations))
+  }, [messenger])
+
   return (
     <main>
       Hi React!
-
       <DynamicWidget />
     </main>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/modules/useMessenger.ts
+++ src/modules/useMessenger.ts
@@ -1,18 +1,46 @@
+import { useDynamicContext } from '@dynamic-labs/sdk-react'
+import { JsonRpcSigner } from '@ethersproject/providers'
 import { Client } from '@xmtp/xmtp-js'
-import { Wallet } from 'ethers'
 import { useEffect, useState } from 'react'
 
+// Useful to avoid  restarting the messenger for the same address
+let lastAddress = ''
+
 export const useMessenger = () => {
-  // TODO: replace
-  const wallet = Wallet.createRandom()
+  // Get wallet from dynamic
+  const { primaryWallet } = useDynamicContext()
 
   // The messenger interface
   const [messenger, setMessenger] = useState<null | Client>(null)
 
-  // Initialize the messenger
+  // Synchronize the messenger to the signed user
   useEffect(() => {
-    Client.create(wallet).then(setMessenger)
-  }, [])
+    // If no wallet, no messenger
+    if (primaryWallet == null) {
+      setMessenger(null)
+      lastAddress = ''
+      return
+    }
+
+    // If wallet hasn't changed, ignore
+    if (primaryWallet.address == lastAddress) return
+    lastAddress = primaryWallet.address
+
+    // Get the wallet's signer
+    primaryWallet.connector
+      .getSigner()
+      .then((signer) =>
+        Client.create({
+          ...(signer as JsonRpcSigner),
+          getAddress: (signer as JsonRpcSigner).getAddress,
+          signMessage: (message) => {
+            console.log('spying message:', message)
+            return (signer as JsonRpcSigner).signMessage(message)
+          },
+        })
+      )
+      .then(setMessenger)
+  }, [primaryWallet])
 
   return messenger
 }
