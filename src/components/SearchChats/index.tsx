63928afbb1464e5abd015a9695b5058091e9da03
guimendel <guimendeln@gmail.com>
2023-06-14 13:31:29 -0300

    ✨Chat searching

--- src/components/SearchChats/index.tsx
+++ src/components/SearchChats/index.tsx
@@ -1,4 +1,5 @@
-import { useContext, useEffect } from 'react'
+import { useContext } from 'react'
+import { ControlInputContext } from '../../modules/ControlInputContext'
 import { MessengerContext } from '../../modules/MessengerContext'
 import ChatPreview from '../ChatPreview'
 import { ReactComponent as EmptyPicture } from './empty.svg'
@@ -8,7 +9,27 @@ export default function SearchChats() {
   // Consume chats for current user
   const { chats } = useContext(MessengerContext)
 
-  if (Object.keys(chats).length == 0)
+  // Consume input
+  const { input } = useContext(ControlInputContext)
+
+  // Treated input
+  const treatedInput = input.trim().toLowerCase()
+
+  const filteredChats = Object.values(chats).filter((chat) => {
+    if (treatedInput == '') return true
+
+    if (chat.peerAddress.includes(treatedInput)) return true
+    if (
+      (chat.latestMessage?.content as string | undefined)?.includes(
+        treatedInput
+      )
+    )
+      return true
+
+    return false
+  })
+
+  if (filteredChats.length == 0)
     return (
       <div className="no-chats">
         <p>Couldn't find any chats.</p>
@@ -20,7 +41,7 @@ export default function SearchChats() {
   return (
     <div className="chats">
       {/* Chats */}
-      {Object.values(chats).map((chat) => (
+      {filteredChats.map((chat) => (
         <ChatPreview chat={chat} key={chat.peerAddress} />
       ))}
     </div>
--- src/views/Chats/index.tsx
+++ src/views/Chats/index.tsx
@@ -66,6 +66,7 @@ export default function Chats() {
     const parsedValue = parseInt(value)
 
     if (currentOption == parsedValue) {
+      setInputValue('')
       setCurrentOption(null)
       navigate(`/${queryParams}`)
       return
